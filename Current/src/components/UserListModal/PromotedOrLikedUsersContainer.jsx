28373ea3c187f22da7fa396bd1a6963df5f185a6
Bezrodnov Aleksey <bezrodnov.aleksey@gmail.com>
2019-07-07 09:17:31 +0400

    fix a styling issue caused by global modal css changes

--- Current/src/components/UserListModal/PromotedOrLikedUsersContainer.jsx
+++ Current/src/components/UserListModal/PromotedOrLikedUsersContainer.jsx
@@ -1,45 +1,43 @@
-import React from 'react'
-import PromotedOrLikedUsersView from './PromotedOrLikedUsersView'
-import { connect } from 'react-redux'
-import { fetchLikedUsers, fetchPromotedUsers } from '../../actions/userPublicationAction'
-import './modal.scss'
+import React from 'react';
+import PromotedOrLikedUsersView from './PromotedOrLikedUsersView';
+import { connect } from 'react-redux';
+import { fetchLikedUsers, fetchPromotedUsers } from '../../actions/userPublicationAction';
+
 class PromotedOrLikedUsersContainer extends React.Component {
-    constructor() {
-        super()
-        this.state = {page:1}
-        this.loadMore = this.loadMore.bind(this)
+  constructor() {
+    super();
+    this.state = { page: 1 };
+    this.loadMore = this.loadMore.bind(this);
+  }
+  loadMore() {
+    if (this.props.type === 'liked' && (!this.props.likedUsers.loading && !this.props.likedUsers.noMoreData)) {
+      this.props.fetchLikedUsers(this.props.id, { 'page': this.state.page + 1 });
+      this.setState({ 'page': this.state.page + 1 });
+    } else if (this.props.type === 'promotes' && (!this.props.promotedUsers.loading && !this.props.promotedUsers.noMoreData)) {
+      this.props.fetchPromotedUsers(this.props.id, { 'page': this.state.page + 1 });
+      this.setState({ 'page': this.state.page + 1 });
     }
-    loadMore() {
-        if(this.props.type === 'liked' &&(!this.props.likedUsers.loading&&!this.props.likedUsers.noMoreData)) {
-            this.props.fetchLikedUsers(this.props.id,{'page':this.state.page + 1})
-            this.setState({'page':this.state.page + 1})
-        }
-        else if(this.props.type === 'promotes' &&(!this.props.promotedUsers.loading&&!this.props.promotedUsers.noMoreData)) {
-            this.props.fetchPromotedUsers(this.props.id,{'page':this.state.page + 1})
-            this.setState({'page':this.state.page + 1})
-        }
+  }
+  componentDidMount() {
+    if (this.props.type === 'liked') {
+      this.props.fetchLikedUsers(this.props.id);
+    } else {
+      this.props.fetchPromotedUsers(this.props.id);
     }
-    componentDidMount() {
-        if (this.props.type === 'liked') {
-            this.props.fetchLikedUsers(this.props.id)
-        }
-        else {
-            this.props.fetchPromotedUsers(this.props.id)
-        }
-    }
-    render() {
-        return (
-            <PromotedOrLikedUsersView loadMore={this.loadMore} {...this.props} />
-        )
-    }
+  }
+  render() {
+    return (
+      <PromotedOrLikedUsersView loadMore={this.loadMore} {...this.props} />
+    );
+  }
 }
 const mapStateToProps = state => ({
-    likedUsers: state.likedUsers,
-    promotedUsers: state.promotedUsers,
+  likedUsers: state.likedUsers,
+  promotedUsers: state.promotedUsers,
 });
 const mapDispatchersToProps = {
-    fetchLikedUsers,
-    fetchPromotedUsers
+  fetchLikedUsers,
+  fetchPromotedUsers,
 };
 
-export default connect(mapStateToProps, mapDispatchersToProps)(PromotedOrLikedUsersContainer)
+export default connect(mapStateToProps, mapDispatchersToProps)(PromotedOrLikedUsersContainer);
--- Current/src/components/UserListModal/PromotedOrLikedUsersView.jsx
+++ Current/src/components/UserListModal/PromotedOrLikedUsersView.jsx
@@ -1,54 +1,68 @@
-import React from 'react'
+import React from 'react';
 import { Modal } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Spinner from 'react-bootstrap/Spinner'
+import Spinner from 'react-bootstrap/Spinner';
 import { BASE_URL } from '../../app.constants';
 
-class PromotedOrLikedUsersView extends React.Component {
-    render() {
-        const { likedUsers, type, promotedUsers } = this.props
-        return (
-            <Modal show={true} onHide={this.props.close}>
-                <Modal.Header closeButton>
-                    <Modal.Title>{type === 'liked' ? 'Liked By' : 'Promoted By'}</Modal.Title>
-                </Modal.Header>
+import './modal.scss';
 
-                <Modal.Body>
-                    {type === 'liked' ? <React.Fragment>{
-                        likedUsers.users.map(
-                            (user, key) => (
-                                <Row key={key} className='user-item-container'>
-                                    <Col className='flex-container' md={4}>
-                                        <span><img className='user-image' src={BASE_URL + user.avatar} alt='avatar' /></span>
-                                        <div className='flex-container flex-direction-column user-details-div'>
-                                            <span>{user.first_name} {user.last_name}</span>
-                                            <span>@{user.email}</span>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            )
-                        )
-                    }
-                        {likedUsers.loading ? <div className='text-center'><Spinner animation="border" /> </div> : (!likedUsers.noMoreData ? <div onClick={this.props.loadMore} className='load-more-link'>Load More</div> : <div className='text-center'>No More Data</div>)}
-                    </React.Fragment> : <React.Fragment>
-                            {promotedUsers.users.map(
-                                (user, key) => (
-                                    <Row key={key} className='user-item-container'>
-                                        <Col className='flex-container' md={4}>
-                                            <span><img className='user-image' src={BASE_URL + user.avatar} alt='avatar' /></span>
-                                            <div className='flex-container flex-direction-column user-details-div'>
-                                                <span>{user.first_name} {user.last_name}</span>
-                                                <span>@{user.email}</span>
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                ))}
-                            {promotedUsers.loading ? <div className='text-center'><Spinner animation="border" /> </div> : (!promotedUsers.noMoreData ? <div onClick={this.props.loadMore} className='load-more-link'>Load More</div> : <div className='text-center'>No More Data</div>)}
-                        </React.Fragment>}
-                </Modal.Body>
-            </Modal>
-        )
-    }
+class PromotedOrLikedUsersView extends React.Component {
+  render() {
+    const { likedUsers, type, promotedUsers } = this.props;
+    return (
+      <Modal show={true} onHide={this.props.close} className="promoted-liked-users-modal">
+        <Modal.Header closeButton>
+          <Modal.Title>{type === 'liked' ? 'Liked By' : 'Promoted By'}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {type === 'liked' ? <React.Fragment>{
+            likedUsers.users.map(
+              (user, key) => (
+                <Row key={key} className="user-item-container">
+                  <Col className="flex-container" md={4}>
+                    <span><img className="user-image" src={BASE_URL + user.avatar} alt="avatar" /></span>
+                    <div className="flex-container flex-direction-column user-details-div">
+                      <span>{user.first_name} {user.last_name}</span>
+                      <span>@{user.email}</span>
+                    </div>
+                  </Col>
+                </Row>
+              )
+            )
+          }
+          {likedUsers.loading
+            ? <div className="text-center"><Spinner animation="border" /></div>
+            : !likedUsers.noMoreData
+              ? <div onClick={this.props.loadMore} className="load-more-link">Load More</div>
+              : <div className="text-center">No More Data</div>
+
+          }
+          </React.Fragment> : <React.Fragment>
+            {promotedUsers.users.map(
+              (user, key) => (
+                <Row key={key} className="user-item-container">
+                  <Col className="flex-container" md={4}>
+                    <span><img className="user-image" src={BASE_URL + user.avatar} alt="avatar" /></span>
+                    <div className="flex-container flex-direction-column user-details-div">
+                      <span>{user.first_name} {user.last_name}</span>
+                      <span>@{user.email}</span>
+                    </div>
+                  </Col>
+                </Row>
+              ))}
+            {promotedUsers.loading
+              ? <div className="text-center"><Spinner animation="border" /></div>
+              : !promotedUsers.noMoreData
+                ? <div onClick={this.props.loadMore} className="load-more-link">Load More</div>
+                : <div className="text-center">No More Data</div>
+            }
+          </React.Fragment>
+          }
+        </Modal.Body>
+      </Modal>
+    );
+  }
 }
-export default PromotedOrLikedUsersView
+export default PromotedOrLikedUsersView;
