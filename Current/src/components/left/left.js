a8b808a583ad434112648cd812994d6ecfe0754a
Harikrishnan <krishnaramahari94@gmail.com>
2019-07-14 09:31:38 +0530

    changes response to array for user list

--- Current/src/components/left/left.js
+++ Current/src/components/left/left.js
@@ -7,26 +7,25 @@ import './left.scss';
 
 export default class Left extends Component {
   render() {
-    console.log('##in left')
-    console.log(this.props.user)
+    const user = this.props.user[0]
     return (
       <div className='left'>
-        <Image src={this.props.user && BASE_URL + this.props.user.avatar} className='left__avatar' />
+        <Image src={user && BASE_URL + user.avatar} className='left__avatar' />
 
         <h3
-          className='left__username'>{this.props.user && (this.props.user.first_name + ' ' + this.props.user.last_name)}</h3>
-        <p className='left__description'>{this.props.user && this.props.user.profession}</p>
+          className='left__username'>{user && (user.first_name + ' ' + user.last_name)}</h3>
+        <p className='left__description'>{user && user.profession}</p>
         <div className='left-meta'>
           <figure className='left-meta__icon'>
             <FontAwesomeIcon icon={faGraduationCap} className='left-meta__fa' />
           </figure>
-          {this.props.user && this.props.user.school}
+          {user && user.school}
         </div>
         <div className='left-meta'>
           <figure className='left-meta__icon'>
             <FontAwesomeIcon icon={faMapMarkerAlt} className='left-meta__fa' />
           </figure>
-          {this.props.user && this.props.user.location}
+          {user && user.location}
         </div>
 
         <section className='people-wrapper'>
