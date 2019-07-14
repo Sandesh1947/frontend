18a9f6d8adba615154acdfc7e2ef3724ee7763c8
Harikrishnan <krishnaramahari94@gmail.com>
2019-07-14 20:50:37 +0530

    added check for user in left page

--- Current/src/components/left/left.js
+++ Current/src/components/left/left.js
@@ -7,73 +7,75 @@ import './left.scss';
 
 export default class Left extends Component {
   render() {
-    const user = this.props.user[0]
+    const user = this.props.user ? this.props.user[0] : null
     return (
-      <div className='left'>
-        <Image src={user && BASE_URL + user.avatar} className='left__avatar' />
+      <React.Fragment>
+        {user && <div className='left'>
+          <Image src={user && BASE_URL + user.avatar} className='left__avatar' />
 
-        <h3
-          className='left__username'>{user && (user.first_name + ' ' + user.last_name)}</h3>
-        <p className='left__description'>{user && user.profession}</p>
-        <div className='left-meta'>
-          <figure className='left-meta__icon'>
-            <FontAwesomeIcon icon={faGraduationCap} className='left-meta__fa' />
-          </figure>
-          {user && user.school}
-        </div>
-        <div className='left-meta'>
-          <figure className='left-meta__icon'>
-            <FontAwesomeIcon icon={faMapMarkerAlt} className='left-meta__fa' />
-          </figure>
-          {user && user.location}
-        </div>
-
-        <section className='people-wrapper'>
-          <div className='people'>
-            {this.props.followers && (
-              <div className='people__heading d-flex flex-row justify-content-between align-items-baseline'>
-                <h6 className='people__title'>Audience</h6>
-                <a href='#' className='text-info people__all'>
-                  <h6>All</h6>
-                </a>
-              </div>
-            )}
-            {this.props.followers && (
-              <div className='people-avatars'>
-                {this.props.followers.map((value, index) => {
-                  return (
-                    <figure className='people-avatars__figure' key={index}>
-                      <Image className='people-avatars__image' src={BASE_URL + value.avatar} />
-                    </figure>
-                  )
-                })}
-              </div>
-            )}
+          <h3
+            className='left__username'>{user && (user.first_name + ' ' + user.last_name)}</h3>
+          <p className='left__description'>{user && user.profession}</p>
+          <div className='left-meta'>
+            <figure className='left-meta__icon'>
+              <FontAwesomeIcon icon={faGraduationCap} className='left-meta__fa' />
+            </figure>
+            {user && user.school}
+          </div>
+          <div className='left-meta'>
+            <figure className='left-meta__icon'>
+              <FontAwesomeIcon icon={faMapMarkerAlt} className='left-meta__fa' />
+            </figure>
+            {user && user.location}
           </div>
 
-          <div className='people'>
-            {this.props.partners && (
-              <div className='people__heading d-flex flex-row justify-content-between align-items-baseline'>
-                <h6 className='people__title'>Partners</h6>
-                <a href='#' className='text-info people__all'>
-                  <h6>All</h6>
-                </a>
-              </div>
-            )}
-            {this.props.partners && (
-              <div className='people-avatars'>
-                {this.props.partners.map((value, index) => {
-                  return (
-                    <figure className='people-avatars__figure' key={index}>
-                      <Image className='people-avatars__image' src={BASE_URL + value.avatar} />
-                    </figure>
-                  )
-                })}
-              </div>
-            )}
-          </div>
-        </section>
-      </div>
+          <section className='people-wrapper'>
+            <div className='people'>
+              {this.props.followers && (
+                <div className='people__heading d-flex flex-row justify-content-between align-items-baseline'>
+                  <h6 className='people__title'>Audience</h6>
+                  <a href='#' className='text-info people__all'>
+                    <h6>All</h6>
+                  </a>
+                </div>
+              )}
+              {this.props.followers && (
+                <div className='people-avatars'>
+                  {this.props.followers.map((value, index) => {
+                    return (
+                      <figure className='people-avatars__figure' key={index}>
+                        <Image className='people-avatars__image' src={BASE_URL + value.avatar} />
+                      </figure>
+                    )
+                  })}
+                </div>
+              )}
+            </div>
+
+            <div className='people'>
+              {this.props.partners && (
+                <div className='people__heading d-flex flex-row justify-content-between align-items-baseline'>
+                  <h6 className='people__title'>Partners</h6>
+                  <a href='#' className='text-info people__all'>
+                    <h6>All</h6>
+                  </a>
+                </div>
+              )}
+              {this.props.partners && (
+                <div className='people-avatars'>
+                  {this.props.partners.map((value, index) => {
+                    return (
+                      <figure className='people-avatars__figure' key={index}>
+                        <Image className='people-avatars__image' src={BASE_URL + value.avatar} />
+                      </figure>
+                    )
+                  })}
+                </div>
+              )}
+            </div>
+          </section>
+        </div>}
+      </React.Fragment>
     )
   }
 }
