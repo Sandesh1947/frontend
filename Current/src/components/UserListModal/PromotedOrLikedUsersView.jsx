eb8e25e2079476c9e02960376ad6ac4329ea5282
Harikrishnan <krishnaramahari94@gmail.com>
2019-07-19 21:20:35 +0530

    added link to profile page in liked/promoted users list

--- Current/src/components/UserListModal/PromotedOrLikedUsersView.jsx
+++ Current/src/components/UserListModal/PromotedOrLikedUsersView.jsx
@@ -1,11 +1,66 @@
 import React from 'react';
-import { Modal } from 'react-bootstrap';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Spinner from 'react-bootstrap/Spinner';
 import { BASE_URL } from '../../app.constants';
-
+import { Link } from 'react-router-dom'
+import { Popover, Overlay, Row, Col, Modal, Spinner } from 'react-bootstrap';
+import queryString from 'query-string';
 import './modal.scss';
+class UserNameFormatter extends React.Component {
+  constructor() {
+    super()
+    this.state = { showPopOver: [] }
+    this.handlePopOver = this.handlePopOver.bind(this)
+    this.handleBodyClick = this.handleBodyClick.bind(this)
+    this.refList = null;
+  }
+  componentDidMount() {
+    document.addEventListener('click', this.handleBodyClick)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleBodyClick);
+  }
+  handleBodyClick(event) {
+    let currentState = this.state.showPopOver
+    if (event.target.id !== 'likedusers') {
+      currentState.forEach((element, key) => {
+        if (currentState[key])
+          currentState[key] = false
+      });
+      this.setState({ showPopOver: currentState })
+    }
+  }
+  handlePopOver(user, key) {
+    if (user.currentuser === 0 && !this.state.showPopOver[key]) {
+      let currentState = this.state.showPopOver
+      currentState[key] = true
+      this.setState({ showPopOver: currentState })
+    }
+    else {
+      let currentState = this.state.showPopOver
+      currentState[key] = false
+      this.setState({ showPopOver: currentState })
+    }
+  }
+  render() {
+    const { user, index } = this.props
+    return (
+      <React.Fragment>
+        <span id='likedusers' className='user-name' ref={refList => this.refList = refList} onClick={() => this.handlePopOver(user, index)} >{user.first_name} {user.last_name}</span>
+        <Overlay
+          show={this.state.showPopOver[index]}
+          target={this.refList}
+          placement="bottom"
+
+        >
+          <Popover id="popover-contained" title="View Profile">
+            <strong><Link to={{ pathname: '/profile/', search: queryString.stringify(Object.assign({}, { user_id: user.user_id })), state: { currentuser: user.currentuser } }}>Click to view profile of {user.first_name}</Link></strong>
+          </Popover>
+        </Overlay>
+      </React.Fragment>
+    )
+  }
+}
+
 
 class PromotedOrLikedUsersView extends React.Component {
   render() {
@@ -24,7 +79,7 @@ class PromotedOrLikedUsersView extends React.Component {
                   <Col className="flex-container" md={4}>
                     <span><img className="user-image" src={BASE_URL + user.avatar} alt="avatar" /></span>
                     <div className="flex-container flex-direction-column user-details-div">
-                      <span>{user.first_name} {user.last_name}</span>
+                      <UserNameFormatter user={user} index={key} />
                       <span>@{user.email}</span>
                     </div>
                   </Col>
@@ -32,33 +87,33 @@ class PromotedOrLikedUsersView extends React.Component {
               )
             )
           }
-          {likedUsers.loading
-            ? <div className="text-center"><Spinner animation="border" /></div>
-            : !likedUsers.noMoreData
-              ? <div onClick={this.props.loadMore} className="load-more-link">Load More</div>
-              : <div className="text-center">No More Data</div>
-
-          }
-          </React.Fragment> : <React.Fragment>
-            {promotedUsers.users.map(
-              (user, key) => (
-                <Row key={key} className="user-item-container">
-                  <Col className="flex-container" md={4}>
-                    <span><img className="user-image" src={BASE_URL + user.avatar} alt="avatar" /></span>
-                    <div className="flex-container flex-direction-column user-details-div">
-                      <span>{user.first_name} {user.last_name}</span>
-                      <span>@{user.email}</span>
-                    </div>
-                  </Col>
-                </Row>
-              ))}
-            {promotedUsers.loading
+            {likedUsers.loading
               ? <div className="text-center"><Spinner animation="border" /></div>
-              : !promotedUsers.noMoreData
+              : !likedUsers.noMoreData
                 ? <div onClick={this.props.loadMore} className="load-more-link">Load More</div>
                 : <div className="text-center">No More Data</div>
+
             }
-          </React.Fragment>
+          </React.Fragment> : <React.Fragment>
+              {promotedUsers.users.map(
+                (user, key) => (
+                  <Row key={key} className="user-item-container">
+                    <Col className="flex-container" md={4}>
+                      <span><img className="user-image" src={BASE_URL + user.avatar} alt="avatar" /></span>
+                      <div className="flex-container flex-direction-column user-details-div">
+                      <UserNameFormatter user={user} index={key} />
+                        <span>@{user.email}</span>
+                      </div>
+                    </Col>
+                  </Row>
+                ))}
+              {promotedUsers.loading
+                ? <div className="text-center"><Spinner animation="border" /></div>
+                : !promotedUsers.noMoreData
+                  ? <div onClick={this.props.loadMore} className="load-more-link">Load More</div>
+                  : <div className="text-center">No More Data</div>
+              }
+            </React.Fragment>
           }
         </Modal.Body>
       </Modal>
