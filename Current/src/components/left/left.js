b2b988d4b3deda351b423efb48433ce9acf17d53
Harikrishnan <krishnaramahari94@gmail.com>
2019-08-03 21:01:05 +0530

    added editing profilepic and coverpic in profile page

--- Current/src/components/left/left.js
+++ Current/src/components/left/left.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
 import { faGraduationCap, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Image from 'react-bootstrap/Image';
+import { Button,Image } from 'react-bootstrap'
 import { BASE_URL } from '../../app.constants';
+import isEqual from 'lodash/isEqual'
 import './left.scss';
 
 export default class Left extends Component {
+  constructor(props){
+    super(props)
+    this.state = {avatar: '',avatarChanged:false}
+    this.onFileUploadAvatar = this.onFileUploadAvatar.bind(this)
+    this.loadImage = this.loadImage.bind(this)
+    this.initAvatar = this.initAvatar.bind(this)
+  }
+  componentDidMount() {
+    this.initAvatar();
+  }
+  componentDidUpdate(prevProps) {
+    if(!isEqual(this.props.user,prevProps.user)) {
+      this.initAvatar()
+    }
+  }
+  initAvatar() {
+    const user = this.props.user ? this.props.user[0] : null
+    if(user)
+    this.setState({avatar : BASE_URL + user.avatar,avatarChanged:false})
+  }
+  onFileUploadAvatar(e) {
+    if (!e.target.files.length) {
+        return;
+    }
+    const attachment = Array.from(e.target.files)[0];
+    this.loadImage(attachment)
+
+}
+loadImage(attachment) {
+  const reader = new FileReader();
+  reader.onload = e => {
+      this.setState({ 'avatar': e.target.result,avatarChanged:true });
+  };
+  reader.readAsDataURL(attachment);
+}
   render() {
     const user = this.props.user ? this.props.user[0] : null
     return (
       <React.Fragment>
         {user && <div className='left'>
-          <Image src={user && BASE_URL + user.avatar} className='left__avatar' />
-
+          {this.props.from === 'profile' ?
+            <React.Fragment>
+              <div className='pic-container'>
+                <label htmlFor='profile-pic'>
+                  <Image src={this.state.avatar} className='left__avatar' />
+                </label>
+                <input
+                  className="d-none"
+                  type="file"
+                  id='profile-pic'
+                  name='profile-pic'
+                  onChange={this.onFileUploadAvatar}
+                />
+              </div>
+              {this.state.avatarChanged && <div className='d-flex'>
+              <Button size="sm" className='mr-3' onClick={() => this.props.editProfile({avatar :this.state.avatar})} variant="outline-success">Save</Button>
+              <Button size="sm" onClick={() => { this.initAvatar() }} variant="outline-danger">Cancel</Button>
+                </div>}
+            </React.Fragment> : <Image src={this.state.avatar} className='left__avatar' />}
           <h3
             className='left__username'>{user && (user.first_name + ' ' + user.last_name)}</h3>
           <p className='left__description'>{user && user.bio}</p>
