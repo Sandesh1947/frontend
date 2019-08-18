a43739992c0ef2df18b18849c1d5ada8a829c35b
Harikrishnan <krishnaramahari94@gmail.com>
2019-08-18 09:02:49 +0530

    bug fix in profilepic overlay

--- Current/src/components/left/profilePic.js
+++ Current/src/components/left/profilePic.js
@@ -2,15 +2,10 @@ import React from 'react'
 import { Modal, Image } from 'react-bootstrap';
 export default class ProfilePic extends React.Component {
     render() {
-        const { imagesrc } = this.props
+        const { imgsrc } = this.props
         return (
             <Modal centered show={true} onHide={this.props.close} >
-                <Modal.Header closeButton>
-                </Modal.Header>
-
-                <Modal.Body>
-                    <Image src={imagesrc} />
-                </Modal.Body>
+             <Image src={imgsrc} />
             </Modal>
         )
     }
