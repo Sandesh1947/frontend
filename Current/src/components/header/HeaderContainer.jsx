b063d806dc54c7cf09f7ee2f7baba58fa21a6141
Harikrishnan <krishnaramahari94@gmail.com>
2019-08-24 09:45:53 +0530

    issue with clearing publications while logout

--- Current/src/components/header/HeaderContainer.jsx
+++ Current/src/components/header/HeaderContainer.jsx
@@ -6,7 +6,7 @@ import queryString from 'query-string';
 import Header from './Header';
 // import { getUserPublications, clearUserPublication } from '../../actions/userPublicationAction';
 import { logout } from '../../actions/accountAction';
-
+import { clearUserPublication } from '../../actions/userPublicationAction'
 class HeaderContainer extends Component {
   // componentDidMount() {
   //   It is better to get user publications at home container and profile container as likes and promotes are done and we need to keep track of it as pages are navigated
@@ -30,7 +30,10 @@ class HeaderContainer extends Component {
       });
     }
   }
-
+  handleLogout = () => {
+    this.props.clearUserPublication()
+    this.props.logout()
+  }
   render() {
     const { userInfo } = this.props;
     return (
@@ -48,7 +51,8 @@ class HeaderContainer extends Component {
 const mapStateToProps = ({ userInfo }) => ({ userInfo });
 
 const mapDispatchersToProps = {
-  logout
+  logout,
+  clearUserPublication
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchersToProps)(HeaderContainer));
